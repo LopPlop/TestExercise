0bdb3be760a128330c0fa2e54cc5e2800d37f171
LopPlop <sedakovzaeba123@gmail.com>
2022-10-19 22:28:48 +0700

    half work done

---
+++ Example/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using GeometryLib;
+
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<GeometricObject> geometricObjects = new List<GeometricObject>();
+
+            string path = @"C:\\Users\\Bruh\\source\\repos\\TestExercise\\Example\\Example.txt";
+            
+            try
+            {
+                using (var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                {
+
+
+                    if (fileStream.Length <= 0)
+                    {
+                        using (var streamWriter = new StreamWriter(fileStream))
+                        {
+                            streamWriter.WriteLine("point 1 2");
+                            streamWriter.WriteLine("rect 1 2 3 4");
+                            streamWriter.WriteLine("line 10 10 20 20");
+                            streamWriter.WriteLine("circle 10 10 5");
+                        }
+                    }
+                }
+
+                using (var streamReader = new StreamReader(path))
+                    {
+                        string str = null;
+
+                        while (!streamReader.EndOfStream)
+                        {
+                            str = streamReader.ReadLine();
+
+                            if (str.Contains("point"))
+                            {
+                                int x, y;
+                                string tmpStr = null;
+                                int i = str.IndexOf(" ") + 1;
+                                while (str[i] != ' ')
+                                {
+                                    tmpStr += str[i];
+                                    if (i != str.Length-1)
+                                    i++;
+                                }
+                                i+=1;
+                                x = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                                while (str[i] != ' ')
+                                {
+                                    tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                                }
+                                y = Int32.Parse(tmpStr);
+                                geometricObjects.Add(new Point(x, y));
+                            }
+
+                            if (str.Contains("rect"))
+                            {
+                            int x1, y1, x2, y2;
+                            string tmpStr = null;
+                            int i = str.IndexOf(" ") + 1;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                            }
+                            i += 1;
+                            x1 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            i += 1;
+                            y1 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                            }
+                            i += 1;
+                            x2 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            i += 1;
+                            y2 = Int32.Parse(tmpStr);
+                            geometricObjects.Add(new Rec(x1, y1, x2, y2));
+                        }
+
+
+
+                            if (str.Contains("line"))
+                            {
+                            int x1, y1, x2, y2;
+                            string tmpStr = null;
+                            int i = str.IndexOf(" ") + 1;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                            }
+                            i += 1;
+                            x1 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            i += 1;
+                            y1 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                            }
+                            i += 1;
+                            x2 = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            i += 1;
+                            y2 = Int32.Parse(tmpStr);
+                            geometricObjects.Add(new Line(x1, y1, x2, y2));
+                        }
+
+
+
+                            if (str.Contains("circle"))
+                            {
+                            int x, y, r;
+                            string tmpStr = null;
+                            int i = str.IndexOf(" ") + 1;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                            }
+                            i += 1;
+                            x = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            i += 1;
+                            y = Int32.Parse(tmpStr);
+                            tmpStr = null;
+                            while (str[i] != ' ')
+                            {
+                                tmpStr += str[i];
+                                if (i != str.Length - 1)
+                                    i++;
+                                else
+                                    break;
+                            }
+                            r = Int32.Parse(tmpStr);
+                            geometricObjects.Add(new Circle(x, y, r));
+                        }
+                        }
+                    }
+                for (int i = 0; i < geometricObjects.Count; i++)
+                    geometricObjects[i].Draw();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ GeometryLib/GeometryClass.cs
@@ -0,0 +1,101 @@
+﻿namespace GeometryLib
+{
+        public abstract class  GeometricObject
+        {
+            public abstract void Draw();
+            public abstract void Intersect();
+        }
+
+        public class Point : GeometricObject
+        {
+            private double x { get; set; }
+            private double y { get; set; }
+            
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override void Draw()
+            {
+                System.Console.WriteLine($"point at ({x},{y})");
+            }
+
+            public override void Intersect()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Rec : GeometricObject
+        {
+            private double x1 { get; set; }
+            private double y1 { get; set; }
+            private double x2 { get; set; }
+            private double y2 { get; set; }
+            public Rec(double x1, double y1, double x2, double y2)
+            {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            }
+
+            public override void Draw()
+            {
+                System.Console.WriteLine($"rect at ({x1},{y1}),({x2},{y2})");
+            }
+
+            public override void Intersect()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Line : GeometricObject
+        {
+            private double x1 { get; set; }
+            private double y1 { get; set; }
+            private double x2 { get; set; }
+            private double y2 { get; set; }
+            public Line(double x1, double y1, double x2, double y2)
+            {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            }
+
+            public override void Draw()
+            {
+                System.Console.WriteLine($"line at ({x1},{y1},{x2},{y2})");
+            }
+
+            public override void Intersect()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Circle : GeometricObject
+        {
+            private double x { get; set; }
+            private double y { get; set; }
+            private double r { get; set; }
+            public Circle(double x, double y, double r)
+            {
+                this.x = x;
+                this.y = y;
+                this.r = r;
+            }
+
+            public override void Draw()
+            {
+                System.Console.WriteLine($"circle at ({x},{y}), radius ({r})");
+            }
+
+            public override void Intersect()
+            {
+                throw new NotImplementedException();
+            }
+        }
+}
